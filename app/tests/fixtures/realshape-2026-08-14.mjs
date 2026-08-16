db14a0414db8ba3a7b22b0296e48a128b92745fc
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-16 09:39:36 +0545

    test: add anonymized real-shape fixture

---
+++ app/tests/fixtures/realshape-2026-08-14.mjs
@@ -0,0 +1,99 @@
+export const REALSHAPE_LOG = String.raw`26:08:14:11:22:28.6::PlayerA,P[100001@1 PlayerA],,*,Epli,C[165 M33_Dn_Basilisk_Boss_2],Life Lessons,Pn.Xr0e3v,Physical,Critical,72613.8,99471
+26:08:14:11:22:28.6::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Radiant Weapon,Pn.101do6,Physical,Dodge,9557.65,10059.4
+26:08:14:11:22:28.8::PlayerC,P[100003@1 PlayerC],,*,,*,Sentinel's Slash,Pn.F4u5b21,Power,,-28.321,0
+26:08:14:11:22:28.8::PlayerC,P[100003@1 PlayerC],,*,,*,Knight's Condemnation,Pn.Bywptk,HitPoints,,-76409.9,-61858.2
+26:08:14:11:22:28.8::PlayerB,P[100002@1 PlayerB],,*,,*,Killing Flames,Pn.Wao6x01,Power,,-17.0308,0
+26:08:14:11:22:28.8::PlayerC,P[100003@1 PlayerC],,*,Epli,C[165 M33_Dn_Basilisk_Boss_2],Sentinel's Slash,Pn.F4u5b21,Physical,Flank,16621.8,22979.5
+26:08:14:11:22:28.8::PlayerC,P[100003@1 PlayerC],,*,Ber,C[163 M33_Dn_Basilisk_Boss_1],Sentinel's Slash,Pn.F4u5b21,Physical,Critical|Dodge|Flank,19861.6,37216.3
+26:08:14:11:22:28.9::PlayerC,P[100003@1 PlayerC],,*,,*,Life Lessons,Pn.Xr0e3v,HitPoints,,-2453.39,-1986.16
+26:08:14:11:22:28.9::PlayerC,P[100003@1 PlayerC],,*,,*,Enhanced Application,Pn.Pxnddv,HitPoints,,-15663.2,0
+26:08:14:11:22:28.9::PlayerC,P[100003@1 PlayerC],,*,,*,Enhanced Application,Pn.Pxnddv,HitPoints,,-78315.9,0
+26:08:14:11:22:28.9::PlayerB,P[100002@1 PlayerB],,*,,*,Dark Helix,Pn.Osjf6l1,Power,,-22.5408,0
+26:08:14:11:22:28.9::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Dark Helix,Pn.Osjf6l1,Physical,Critical|Dodge|Flank,477882,682766
+26:08:14:11:22:28.9::PlayerC,P[100003@1 PlayerC],,*,Ber,C[163 M33_Dn_Basilisk_Boss_1],Life Lessons,Pn.Xr0e3v,Physical,Critical|Flank,55287.4,69677.2
+26:08:14:11:22:29.0::PlayerB,P[100002@1 PlayerB],Companion1,C[200001 Scourge_Soulwraith],Hunang,C[164 M33_Dn_Remorhaz_Boss],Wraith Claw,Pn.8s87af,Physical,Critical|Dodge|Flank,170287,237516
+26:08:14:11:22:29.0::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Radiant Weapon,Pn.101do6,Physical,,9557.65,0
+26:08:14:11:22:29.0::PlayerA,P[100001@1 PlayerA],Split the Sky,C[171 Entity_Splitthesky],Hunang,C[164 M33_Dn_Remorhaz_Boss],Split the Sky,Pn.Brlco6,Physical,Critical|Flank|DoT,664069,901451
+26:08:14:11:22:29.0::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Creeping Death,Pn.Kkbsma1,Physical,Critical|Flank|DoT,327845,445038
+26:08:14:11:22:29.1::PlayerD,P[100004@1 PlayerD],,*,Epli,C[165 M33_Dn_Basilisk_Boss_2],Rimefire Smolder,Pn.Bomdua,Physical,Dodge|Flank|DoT,159046,230415
+26:08:14:11:22:29.1::PlayerA,P[100001@1 PlayerA],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Radiant Weapon,Pn.101do6,Physical,,13281.4,0
+26:08:14:11:22:29.1::PlayerD,P[100004@1 PlayerD],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Glowing Flames,Pn.R4sli2,Physical,Dodge|Flank|DoT,57959.2,81311.3
+26:08:14:11:22:29.1::PlayerD,P[100004@1 PlayerD],,*,Ber,C[163 M33_Dn_Basilisk_Boss_1],Glowing Flames,Pn.R4sli2,Physical,Dodge|Flank|DoT,58493.7,78764.5
+26:08:14:11:22:29.2::PlayerD,P[100004@1 PlayerD],Epli,C[165 M33_Dn_Basilisk_Boss_2],PlayerD,P[100004@1 PlayerD],Smoldering Recovery,Pn.E693wi,Power,,-5,0
+26:08:14:11:22:29.3::,,,,PlayerC,P[100003@1 PlayerC],Frostbite,Pn.W5du661,Shield,,-10155.8,0
+26:08:14:11:22:29.3::PlayerC,P[100003@1 PlayerC],,*,,*,Block,Pn.Fk1hm3,Power,,-6.32164,0
+26:08:14:11:22:29.3::,,,,PlayerD,P[100004@1 PlayerD],Frostbite,Pn.W5du661,Shield,ShieldBreak,-11500.7,0
+26:08:14:11:22:29.3::,,,,PlayerD,P[100004@1 PlayerD],Frostbite,Pn.W5du661,Shield,ShieldBreak,-4889.16,0
+26:08:14:11:22:29.3::,,,,PlayerD,P[100004@1 PlayerD],Frostbite,Pn.W5du661,Physical,ShieldBreak,16389.9,21289.7
+26:08:14:11:22:29.3::,,,,PlayerE,P[100005@1 PlayerE],Frostbite,Pn.W5du661,Shield,ShieldBreak,-17988.6,0
+26:08:14:11:22:29.3::,,,,PlayerE,P[100005@1 PlayerE],Frostbite,Pn.W5du661,Shield,ShieldBreak,-485.545,0
+26:08:14:11:22:29.3::,,,,PlayerB,P[100002@1 PlayerB],Frostbite,Pn.W5du661,Shield,,-16483.8,0
+26:08:14:11:22:29.3::,,,,PlayerB,P[100002@1 PlayerB],Frostbite,Pn.W5du661,Physical,,16483.8,21598.4
+26:08:14:11:22:29.3::,,,,PlayerA,P[100001@1 PlayerA],Frostbite,Pn.W5du661,Shield,ShieldBreak,-13062.5,0
+26:08:14:11:22:29.3::,,,,PlayerA,P[100001@1 PlayerA],Frostbite,Pn.W5du661,Shield,ShieldBreak,-1946.19,0
+26:08:14:11:22:29.3::,,,,PlayerA,P[100001@1 PlayerA],Frostbite,Pn.W5du661,Physical,ShieldBreak,15008.6,19573.7
+26:08:14:11:22:29.3::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Killing Flames,Pn.Wao6x01,Physical,Critical|Dodge|Flank,2.29566e+06,3.27989e+06
+26:08:14:11:22:29.4::,,,,PlayerC,P[100003@1 PlayerC],Frostbite,Pn.W5du661,Shield,,-10155.8,0
+26:08:14:11:22:29.4::,,,,PlayerD,P[100004@1 PlayerD],Frostbite,Pn.W5du661,Shield,,-16540.9,0
+26:08:14:11:22:29.4::,,,,PlayerD,P[100004@1 PlayerD],Frostbite,Pn.W5du661,Physical,,16540.9,21485.9
+26:08:14:11:22:29.4::,,,,PlayerE,P[100005@1 PlayerE],Frostbite,Pn.W5du661,Shield,ShieldBreak,-17503.1,0
+26:08:14:11:22:29.4::,,,,PlayerE,P[100005@1 PlayerE],Frostbite,Pn.W5du661,Shield,ShieldBreak,-1105.48,0
+26:08:14:11:22:29.4::PlayerB,P[100002@1 PlayerB],,*,,*,Blood Lust,Pn.Dgqeju,Power,ShowPowerDisplayName,1.015,0
+26:08:14:11:22:29.4::PlayerB,P[100002@1 PlayerB],,*,,*,Vampiric Embrace,Pn.A42dkm1,Power,,-15.0272,0
+26:08:14:11:22:29.4::,,,,PlayerB,P[100002@1 PlayerB],Frostbite,Pn.W5du661,Shield,,-13263.7,0
+26:08:14:11:22:29.4::,,,,PlayerB,P[100002@1 PlayerB],Frostbite,Pn.W5du661,Physical,,13263.7,17379.2
+26:08:14:11:22:29.4::,,,,PlayerA,P[100001@1 PlayerA],Frostbite,Pn.W5du661,Shield,,-15024,0
+26:08:14:11:22:29.4::,,,,PlayerA,P[100001@1 PlayerA],Frostbite,Pn.W5du661,Physical,,15024,19593.7
+26:08:14:11:22:29.4::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Blood Lust,Pn.Dgqeju,Physical,ShowPowerDisplayName,0.03,0
+26:08:14:11:22:29.4::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Radiant Weapon,Pn.101do6,Physical,Dodge,44684.4,45913.2
+26:08:14:11:22:29.4::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Blood Lust,Pn.Dgqeju,Physical,Critical|Flank,478249,632048
+26:08:14:11:22:29.4::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Vampiric Embrace,Pn.A42dkm1,Physical,Critical|Dodge|Flank,1.89021e+06,2.56677e+06
+26:08:14:11:22:29.4::PlayerA,P[100001@1 PlayerA],Split the Sky,C[171 Entity_Splitthesky],Hunang,C[164 M33_Dn_Remorhaz_Boss],Split the Sky,Pn.Brlco6,SpeedRunning,Immune|DoT,0,0
+26:08:14:11:22:29.5::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Radiant Weapon,Pn.101do6,Physical,Dodge,36792.4,37804.2
+26:08:14:11:22:29.8::PlayerC,P[100003@1 PlayerC],,*,,*,Enhanced Application,Pn.Pxnddv,HitPoints,,-15663.2,0
+26:08:14:11:22:29.8::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Hellish Rebuke,Pn.Nzeonq,Physical,Critical|Dodge|Flank|DoT,39888.5,61150.7
+26:08:14:11:22:29.8::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Life Lessons,Pn.Xr0e3v,Physical,Critical|Flank,232773,307630
+26:08:14:11:22:29.9::PlayerC,P[100003@1 PlayerC],,*,,*,Sure Strike,Pn.P4ekhr,Power,,-11.379,0
+26:08:14:11:22:29.9::PlayerC,P[100003@1 PlayerC],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Sure Strike,Pn.P4ekhr,Physical,Flank,14264,19362.9
+26:08:14:11:22:29.9::PlayerB,P[100002@1 PlayerB],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Radiant Weapon,Pn.101do6,Physical,Dodge,795.781,797.771
+26:08:14:11:22:30.0::Ber,C[163 M33_Dn_Basilisk_Boss_1],,*,PlayerC,P[100003@1 PlayerC],Poison,Pn.Gp1is2,Shield,ShieldBreak|DoT,-4431.61,0
+26:08:14:11:22:30.0::Ber,C[163 M33_Dn_Basilisk_Boss_1],,*,PlayerC,P[100003@1 PlayerC],Poison,Pn.Gp1is2,Shield,ShieldBreak|DoT,-18796.6,0
+26:08:14:11:22:30.0::PlayerC,P[100003@1 PlayerC],,*,,*,Power at Any Cost,Pn.H5slen1,Poison,,78315.9,0
+26:08:14:11:22:30.0::Ber,C[163 M33_Dn_Basilisk_Boss_1],,*,PlayerC,P[100003@1 PlayerC],Poison,Pn.Gp1is2,Physical,Dodge|Flank|ShieldBreak|DoT,23228.2,122835
+26:08:14:11:22:30.0::PlayerC,P[100003@1 PlayerC],Hunang,C[164 M33_Dn_Remorhaz_Boss],,*,Power at Any Cost,Pn.F77il21,Physical,,115385,156632
+26:08:14:11:22:30.0::PlayerC,P[100003@1 PlayerC],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Paralyze,Pn.Njk5io1,ApplyPower,Immune,0,0
+26:08:14:11:22:30.1::PlayerD,P[100004@1 PlayerD],,*,Ber,C[163 M33_Dn_Basilisk_Boss_1],Smolder,Pn.Htcyc51,Physical,Critical|Dodge|DoT,188691,264645
+26:08:14:11:22:30.2::PlayerD,P[100004@1 PlayerD],Ber,C[163 M33_Dn_Basilisk_Boss_1],PlayerD,P[100004@1 PlayerD],Smoldering Recovery,Pn.E693wi,Power,,-5,0
+26:08:14:11:22:30.2::PlayerD,P[100004@1 PlayerD],,*,Epli,C[165 M33_Dn_Basilisk_Boss_2],Glowing Flames,Pn.R4sli2,Physical,Critical|DoT,59151.6,74875.5
+26:08:14:11:22:30.2::PlayerD,P[100004@1 PlayerD],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Glowing Flames,Pn.R4sli2,Physical,Critical|Dodge|Flank|DoT,119279,167337
+26:08:14:11:22:30.2::PlayerD,P[100004@1 PlayerD],,*,Hunang,C[164 M33_Dn_Remorhaz_Boss],Rimefire Smolder,Pn.Bomdua,Physical,Critical|Dodge|Flank|DoT,470786,660468
+26:08:14:11:22:30.2::PlayerD,P[100004@1 PlayerD],,*,Ber,C[163 M33_Dn_Basilisk_Boss_1],Glowing Flames,Pn.R4sli2,Physical,Critical|Flank|DoT,135380,173790
+26:08:14:11:22:30.3::,,,,PlayerC,P[100003@1 PlayerC],Frostbite,Pn.W5du661,Shield,,-14431.5,0
+26:08:14:11:22:30.3::PlayerE,P[100005@1 PlayerE],Companion2,C[200002 Warlock_Soulweaver_Soulspark_Hell],PlayerC,P[100003@1 PlayerC],Inspirit,Pn.B0b46j,HitPoints,,-108322,-87692.8
+26:08:14:11:22:30.3::,,,,PlayerC,P[100003@1 PlayerC],Frostbite,Pn.W5du661,Physical,,14431.5,37114.9
+26:08:14:11:22:30.3::PlayerD,P[100004@1 PlayerD],Hunang,C[164 M33_Dn_Remorhaz_Boss],PlayerD,P[100004@1 PlayerD],Smoldering Recovery,Pn.E693wi,Power,,-5,0
+26:08:14:11:22:30.3::,,,,PlayerD,P[100004@1 PlayerD],Frostbite,Pn.W5du661,Shield,,-17875.3,0
+26:08:14:11:22:30.3::,,,,PlayerD,P[100004@1 PlayerD],Frostbite,Pn.W5du661,Physical,,17875.3,23219.2
+26:08:14:11:22:30.3::,,,,PlayerE,P[100005@1 PlayerE],Frostbite,Pn.W5du661,Shield,ShieldBreak,-16883.1,0
+26:08:14:11:22:30.3::,,,,PlayerE,P[100005@1 PlayerE],Frostbite,Pn.W5du661,Shield,ShieldBreak,-199.77,0
+26:08:14:11:22:30.3::,,,,PlayerB,P[100002@1 PlayerB],Frostbite,Pn.W5du661,Shield,,-16188.1,0
+26:08:14:11:22:30.3::,,,,PlayerB,P[100002@1 PlayerB],Frostbite,Pn.W5du661,Physical,,16188.1,21211
+`;
+
+export const REALSHAPE_EXPECTED = {
+  "contract": "canonical Physical damage; positive amount; exclude ShowPowerDisplayName/Immune; player owner; 5s combat gap; 15s same-boss phase merge",
+  "encounters": [{ "bosses": ["Ber", "Epli", "Hunang"], "damage": 8234352.881, "duration": 1.5999999999985448, "end": 1.5999999999985448, "hits": 29, "start": 0.0, "type": "boss" }],
+  "fixtureVersion": 1,
+  "group": { "damage": 8234352.881, "duration": 1.5999999999985448, "hits": 29 },
+  "lines": 80,
+  "nonCanonicalPositiveTypes": { "Poison": 1 },
+  "players": {
+    "P[100001@1 PlayerA]": { "ca": 1, "caRate": 33.333333333333336, "companion": 0.0, "companionShare": 0.0, "crit": 2, "critRate": 66.66666666666667, "damage": 749964.2000000001, "dps": 749964.2000000001, "duration": 0.5, "hits": 3, "maxHit": 664069.0, "maxPower": "Split the Sky", "name": "PlayerA" },
+    "P[100002@1 PlayerB]": { "ca": 8, "caRate": 61.53846153846154, "companion": 170287.0, "companionShare": 2.8314239444744898, "crit": 8, "critRate": 61.53846153846154, "damage": 6014182.381, "dps": 4626294.139220412, "duration": 1.3000000000029104, "hits": 13, "maxHit": 2295660.0, "maxPower": "Killing Flames", "name": "PlayerB" },
+    "P[100003@1 PlayerC]": { "ca": 4, "caRate": 80.0, "companion": 0.0, "companionShare": 0.0, "crit": 2, "critRate": 40.0, "damage": 221419.8, "dps": 184516.5000004475, "duration": 1.1999999999970896, "hits": 5, "maxHit": 115385.0, "maxPower": "Power at Any Cost", "name": "PlayerC" },
+    "P[100004@1 PlayerD]": { "ca": 6, "caRate": 75.0, "companion": 0.0, "companionShare": 0.0, "crit": 5, "critRate": 62.5, "damage": 1248786.5, "dps": 1135260.4545469563, "duration": 1.0999999999985448, "hits": 8, "maxHit": 470786.0, "maxPower": "Rimefire Smolder", "name": "PlayerD" }
+  },
+  "rejected": 0,
+  "source": "anonymized real-shape excerpt from user-supplied 2026-08-14 Neverwinter combat log",
+  "validDamageRows": 29
+};
